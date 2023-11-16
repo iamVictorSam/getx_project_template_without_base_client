6bdf135053b104748bd6aab1d2abf0aa0e655dc3
Sam Victor <iamvictorsam@gmail.com>
2023-11-16 21:51:20 +0100

    init

---
+++ android/app/src/main/kotlin/com/example/getx_project_template_without_base_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getx_project_template_without_base_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:getx_project_template_without_base_client/presentation/screens/splash/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      builder: (context, child) {
+        return GetMaterialApp(
+          // this where the light mode colors are defined in
+          debugShowCheckedModeBanner: false,
+          title: 'Project Template',
+          home: child,
+        );
+      },
+      child: const SplashScreen(),
+
+      // child: const Dummy(),
+    );
+  }
+}
